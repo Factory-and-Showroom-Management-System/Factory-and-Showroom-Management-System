f61996c9669f04a80f2a2f5e2ca6bf1f988faac1
DHANANJAYA RUPASINGHE <106276257+Dhananjaya0216@users.noreply.github.com>
2024-04-28 12:01:39 +0530

    Refactor UserLogin component, add video background, update logo and styling

--- project/src/components/login/UserLogin.jsx
+++ project/src/components/login/UserLogin.jsx
@@ -1,3 +1,122 @@
+// import React, { useState } from "react";
+// import { useNavigate } from 'react-router-dom';
+// import { FaUserShield } from 'react-icons/fa';
+// import { BsFillShieldLockFill } from 'react-icons/bs';
+// import { AiOutlineSwapRight } from 'react-icons/ai';
+// import video from '../../assets/login/videotest.mp4';
+// import logo from '../../assets/login/mainLogo.png';
+// import '../../styles/login.css';
+
+
+
+// export default function UserLogin() {
+//   const [formData, setFormData] = useState({});
+//   const navigate = useNavigate();
+//   const [loading, setLoading] = useState(false);
+ 
+
+//   const handleChange = (e) => {
+//     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     setLoading(true);
+
+//     try {
+//       const res = await fetch("http://localhost:3000/users/login", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify(formData),
+//       });
+
+     
+
+//       if (res.ok) {
+//         const data = await res.json();
+//         // Redirect based on user role
+//         if (data.role === 'admin') {
+//           navigate('/admin/dashboard');
+//         }else if (data.role === 'sales_manager') {
+//             navigate('/sales/dashboard');}
+//         //   } else if (data.role === 'finance_manager') {
+//         //     navigate('');
+//         //   } else if (data.role === 'inventory_manager') {
+//         //     navigate('');
+//         //   }else  (data.role === 'hr_manager') 
+//         //     navigate('');
+          
+          
+//       } else {
+//         // Handle login failure
+//         console.error("Login failed");
+//       }
+//     } catch (error) {
+//       console.error("Error:", error);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   return (
+//     <div className='flex loginPage'>
+//     <div className="container flex">
+//       <div className="videoDiv">
+//         <video src={video} autoPlay muted loop ></video>
+//       </div>
+
+//       <div className="flex formDiv">
+//         <div className="headerDiv">
+//           <img src={logo} alt="logo" />
+//           <h3>Welcome Back!</h3>
+//         </div>
+//           <div className="">
+//             <form onSubmit={handleSubmit} className='grid form' >
+           
+//               <div className="">
+//                 <label htmlFor="username" className="">Username</label>
+//                 <div className="flex input">
+//                 <FaUserShield className='icon' />
+//                   <input
+//                     type="text"
+//                     className="input"
+//                     id="username"
+//                     onChange={handleChange}
+//                     placeholder="Enter your username"
+//                   />
+//                 </div>
+//               </div>
+//               <div className="">
+//                 <label htmlFor="password" className="">Password</label>
+//                 <div className="flex input">
+//                 <BsFillShieldLockFill className='icon' />
+//                   <input
+//                     type="password"
+//                     className="input"
+//                     id="password"
+//                     placeholder="********"
+//                     onChange={handleChange}
+//                   />
+//                 </div>
+//               </div>
+//               <div className="">
+//                 <button
+//                   type="submit"
+//                   className='flex btn'
+//                   disabled={loading}
+//                 >
+//                   {loading ? 'Logging in...' : 'Login'}
+//                   <AiOutlineSwapRight className='icon' />
+//                 </button>
+//               </div>
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { FaUserShield } from 'react-icons/fa';
@@ -7,11 +126,11 @@ import video from '../../assets/login/videotest.mp4';
 import logo from '../../assets/login/mainLogo.png';
 import '../../styles/login.css';
 
-
 export default function UserLogin() {
   const [formData, setFormData] = useState({});
-  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -33,22 +152,16 @@ export default function UserLogin() {
         // Redirect based on user role
         if (data.role === 'admin') {
           navigate('/admin/dashboard');
-        }else if (data.role === 'sales_manager') {
-            navigate('/sales/dashboard');}
-        //   } else if (data.role === 'finance_manager') {
-        //     navigate('');
-        //   } else if (data.role === 'inventory_manager') {
-        //     navigate('');
-        //   }else  (data.role === 'hr_manager') 
-        //     navigate('');
-          
-          
+        } else if (data.role === 'sales_manager') {
+          navigate('/sales/dashboard');
+        }
       } else {
         // Handle login failure
-        console.error("Login failed");
+        setError('Incorrect username or password.');
       }
     } catch (error) {
       console.error("Error:", error);
+      setError('An error occurred. Please try again later.');
     } finally {
       setLoading(false);
     }
@@ -56,22 +169,22 @@ export default function UserLogin() {
 
   return (
     <div className='flex loginPage'>
-    <div className="container flex">
-      <div className="videoDiv">
-        <video src={video} autoPlay muted loop ></video>
-      </div>
-
-      <div className="flex formDiv">
-        <div className="headerDiv">
-          <img src={logo} alt="logo" />
-          <h3>Welcome Back!</h3>
+      <div className="container flex">
+        <div className="videoDiv">
+          <video src={video} autoPlay muted loop ></video>
         </div>
+
+        <div className="flex formDiv">
+          <div className="headerDiv">
+            <img src={logo} alt="logo" />
+            <h3>Welcome Back!</h3>
+          </div>
           <div className="">
             <form onSubmit={handleSubmit} className='grid form' >
               <div className="">
                 <label htmlFor="username" className="">Username</label>
                 <div className="flex input">
-                <FaUserShield className='icon' />
+                  <FaUserShield className='icon' />
                   <input
                     type="text"
                     className="input"
@@ -84,7 +197,7 @@ export default function UserLogin() {
               <div className="">
                 <label htmlFor="password" className="">Password</label>
                 <div className="flex input">
-                <BsFillShieldLockFill className='icon' />
+                  <BsFillShieldLockFill className='icon' />
                   <input
                     type="password"
                     className="input"
@@ -104,6 +217,7 @@ export default function UserLogin() {
                   <AiOutlineSwapRight className='icon' />
                 </button>
               </div>
+              {error && <p className="error">{error}</p>}
             </form>
           </div>
         </div>
