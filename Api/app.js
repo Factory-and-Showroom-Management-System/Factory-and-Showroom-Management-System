017f1a7b5d7872a2ababa2cd37dff0aaae6368f2
Dhananjaya Rupasinghe <106276257+GWMDhananjaya@users.noreply.github.com>
2024-05-21 19:49:17 +0530

    add style to admin part

--- Api/app.js
+++ Api/app.js
@@ -8,21 +8,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-
-
 const usersRouter = require('./routes/user');
 const testRouter = require('./routes/tests');
-// const attendanceRouter = require('./routes/attendance');
-// const salaryRouter = require('./routes/salary');
+
 const adminProductController = require('./routes/admin_product');
-
-
 
 app.use('/adminproduct', adminProductController);
 app.use ("/users", usersRouter);
 app.use('/tests', testRouter);
-// app.use('/attendance', attendanceRouter);
-// app.use('/salary', salaryRouter);
 
 
 
--- Front/src/components/sidebar/admin_sidebar.jsx
+++ Front/src/components/sidebar/admin_sidebar.jsx
@@ -1,33 +1,27 @@
 import { Sidebar } from "flowbite-react";
-import {
-  HiShoppingBag,
-  HiUser,
-} from "react-icons/hi";
+import { HiShoppingBag, HiUser } from "react-icons/hi";
 import { Link } from "react-router-dom";
-import { MdOutlineProductionQuantityLimits } from "react-icons/md";
-import { useState } from "react";
 import { GoDatabase } from "react-icons/go";
+import { useState } from "react";
 import { signOutSuccess } from "../../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
-
-
 export function Admin_Sidebar() {
-
   const [dashboardClicked, setDashboardClicked] = useState(false);
   const [productClicked, setProductClicked] = useState(false);
   const [materialClicked, setMaterialClicked] = useState(false);
   const [showModal, setShowModal] = useState(false); // State to manage modal visibility
   const [logoutClicked, setLogoutClicked] = useState(false);
+  const [InventoryClicked, setInventoryClicked] = useState(false);
   const dispatch = useDispatch();
-  const [token, setToken] = useState(localStorage.getItem('token'));
+  const [token, setToken] = useState(localStorage.getItem("token"));
 
   const handleDashboardClick = () => {
     setDashboardClicked(true);
     setProductClicked(false);
     setMaterialClicked(false);
     setLogoutClicked(false);
-     // Ensure other button color is reset
+    setInventoryClicked(false);
   };
 
   const handleProductClick = () => {
@@ -35,6 +29,7 @@ export function Admin_Sidebar() {
     setDashboardClicked(false);
     setMaterialClicked(false);
     setLogoutClicked(false);
+    setInventoryClicked(false);
   };
 
   const handleMaterialClick = () => {
@@ -42,92 +37,138 @@ export function Admin_Sidebar() {
     setDashboardClicked(false);
     setMaterialClicked(true);
     setLogoutClicked(false);
+    setInventoryClicked(false);
   };
 
   const handleSignoutClick = () => {
-    setShowModal(true); // Show the modal on log out button click
+    setShowModal(true);
+    setProductClicked(false);
+    setDashboardClicked(false);
+    setMaterialClicked(false);
+    setLogoutClicked(true);
+    setInventoryClicked(false);
+    
+    
   };
+
+ 
 
   const handleSignoutConfirm = async () => {
     try {
-  
       const res = await fetch("http://localhost:3000/users/signout", {
         method: "POST",
-        
       });
       const data = await res.json();
       if (!res.ok) {
         console.log(data.message);
-        
-      }else {
+      } else {
         dispatch(signOutSuccess());
       }
-      
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
 
   const handleSignoutCancel = () => {
     setShowModal(false); // Hide the modal on cancel
   };
-  
 
-  
   return (
-    <div>
-    <Sidebar className="w-56 border-r-2">
-      <Sidebar.Items className="mb-2">
-        <Sidebar.ItemGroup className="">
-          <div className="mb-2">
-        <Link to="/admin?tab=admindash" >
-          <Sidebar.Item href="" icon={HiUser} className={` rounded-full  hover:bg-[#cdf8da] ${dashboardClicked ? "bg-[#cdf8da] text-black" : ""}`} onClick={handleDashboardClick}>
-          Admin Dashboard
-          </Sidebar.Item>
-          </Link>
-          </div>
-          
-          <div className="mb-2">
-          <Link to="/admin?tab=adminproduct">
-          <Sidebar.Item icon={HiShoppingBag} className={` rounded-full hover:bg-[#cdf8da] ${productClicked ? "bg-[#cdf8da] text-black" : ""}`} onClick={handleProductClick}>
-            Product
-          </Sidebar.Item>
-          </Link>
-          </div>
+    <div className="flex flex-col h-screen">
+      <Sidebar className="w-full border-r-2">
+        <Sidebar.Items className="w-full">
+          <Sidebar.ItemGroup className="w-full">
+            <div className="w-full">
+              <Link to="/admin?tab=admindash">
+                <Sidebar.Item
+                  href=""
+                  icon={HiUser}
+                  className={`rounded-full hover:bg-[#cdf8da] ${
+                    dashboardClicked ? "bg-[#cdf8da] text-black" : ""
+                  }`}
+                  onClick={handleDashboardClick}
+                >
+                  Admin Dashboard
+                </Sidebar.Item>
+              </Link>
+            </div>
 
-         
-          <div className="mb-2">
-          <Link to="/admin?tab=adminmaterial">
-          <Sidebar.Item icon={GoDatabase} className={` rounded-full hover:bg-[#cdf8da] ${materialClicked ? "bg-[#cdf8da] text-black" : ""}`} onClick={handleMaterialClick}>
-            Row Material
-          </Sidebar.Item>
-          </Link>
-          </div>
-         
-        </Sidebar.ItemGroup>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item icon={GoDatabase} className={` rounded-full  hover:bg-[#cdf8da] cursor-pointer ${logoutClicked ? "bg-[#cdf8da] text-black" : ""}`} onClick={handleSignoutClick}>Logout</Sidebar.Item>
-        </Sidebar.ItemGroup>
-        {/* <SignoutModal isOpen={showModal} onCancel={() => setShowModal(false)} onConfirm={confirmSignout} /> */}
-      </Sidebar.Items>
-    </Sidebar>
-    {showModal && (
-      <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
-        <div className="overflow-hidden bg-white rounded-lg shadow-lg w-96">
-          <div className="p-4">
-            <h2 className="mb-4 text-xl font-bold">Do you want to log out?</h2>
-            <div className="flex justify-end space-x-4">
-              <button className="px-4 py-2 font-bold text-white bg-gray-500 rounded cursor-pointer" onClick={handleSignoutCancel}>
-                No
-              </button>
-              <button className="px-4 py-2 font-bold text-white bg-red-500 rounded cursor-pointer" onClick={handleSignoutConfirm}>
-                Yes
-              </button>
+            <Sidebar.Collapse icon={HiShoppingBag} className={`rounded-full hover:bg-[#cdf8da] ${
+                    InventoryClicked ? "bg-[#cdf8da] text-black" : ""
+                  }`}
+                   label="Inventory" >
+
+            <div className="w-full">
+              <Link to="/admin?tab=adminproduct">
+                <Sidebar.Item
+                  icon={HiShoppingBag}
+                  className={`rounded-full hover:bg-[#cdf8da] ${
+                    productClicked ? "bg-[#cdf8da] text-black" : ""
+                  }`}
+                  onClick={handleProductClick}
+                >
+                  Product
+                </Sidebar.Item>
+              </Link>
+            </div>
+
+            <div className="w-full">
+              <Link to="/admin?tab=adminmaterial">
+                <Sidebar.Item
+                  icon={GoDatabase}
+                  className={`rounded-full hover:bg-[#cdf8da] ${
+                    materialClicked ? "bg-[#cdf8da] text-black" : ""
+                  }`}
+                  onClick={handleMaterialClick}
+                >
+                  Raw Material
+                </Sidebar.Item>
+              </Link>
+            </div>
+            
+            </Sidebar.Collapse>
+            
+          </Sidebar.ItemGroup>
+        </Sidebar.Items>
+       
+          
+            <Sidebar.ItemGroup>
+              <Sidebar.Item
+                icon={GoDatabase}
+                className={`rounded-full hover:bg-[#cdf8da] cursor-pointer ${
+                  logoutClicked ? "bg-[#cdf8da] text-black" : ""
+                }`}
+                onClick={handleSignoutClick}
+              >
+                Logout
+              </Sidebar.Item>
+            </Sidebar.ItemGroup>
+     
+      
+      </Sidebar>
+      {showModal && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center pt-10 bg-gray-900 bg-opacity-50">
+          <div className="overflow-hidden bg-white rounded-lg shadow-lg w-96">
+            <div className="p-4">
+              <h2 className="mb-4 text-xl font-bold">Do you want to log out?</h2>
+              <div className="flex justify-end space-x-4">
+                <button
+                  className="px-4 py-2 font-bold text-white bg-gray-500 rounded cursor-pointer"
+                  onClick={handleSignoutCancel}
+                >
+                  No
+                </button>
+                <button
+                  className="px-4 py-2 font-bold text-white bg-red-500 rounded cursor-pointer"
+                  onClick={handleSignoutConfirm}
+                >
+                  Yes
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    )}
+      )}
     </div>
   );
 }
