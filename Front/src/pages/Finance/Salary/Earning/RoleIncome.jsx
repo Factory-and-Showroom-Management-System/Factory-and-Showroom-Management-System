cb1c1af491d25f71cbb3a0cd367686d94e0731db
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2024-07-21 13:34:29 +0530

    All eaning tables color changed

--- Front/src/pages/Finance/Salary/Earning/RoleIncome.jsx
+++ Front/src/pages/Finance/Salary/Earning/RoleIncome.jsx
@@ -401,7 +401,7 @@ export default function RoleIncome() {
 
                 <div className='p-5 '>
 
-                    <h1 className="text-3xl text-blue-500 pl-1 pt-2">Role Income Table</h1>
+                    <h1 className="text-3xl text-green-700 pl-1 pt-2">Role Income Table</h1>
 
                     <div className='mb-2 mt-5 flex items-center '>
 
@@ -450,7 +450,7 @@ export default function RoleIncome() {
 
                     <div className="relative overflow-x-auto sm:rounded-lg">
                         <table className="w-full text-sm text-left text-gray-500 ">
-                            <thead className="text-xs text-white uppercase bg-blue-600 ">
+                            <thead className="text-xs text-white uppercase bg-green-600 ">
                                 <tr>
                                     <th scope="col" className="px-6 py-7">ID</th>
                                     <th scope="col" className="px-6 py-3">Role</th>
@@ -460,7 +460,7 @@ export default function RoleIncome() {
                             </thead>
                             <tbody>
                                 {currentRows.map((income) => (
-                                    <tr key={income.id} className="bg-blue-500 text-white border-b border-blue-400 hover:bg-blue-400">
+                                    <tr key={income.id} className="bg-[#cdf8da] text-black border-b border-[#4bf885] hover:bg-[#a1f0c6]">
                                         <td className="px-6 py-4">{income.id}</td>
                                         <td className="px-6 py-4">{income.role}</td>
                                         <td className="px-6 py-4">Rs. {income.dateIncome}</td>
@@ -471,12 +471,12 @@ export default function RoleIncome() {
                                         </td>
                                     </tr>
                                 ))}
-                                <tr className="bg-blue-800 text-white">
+                                <tr className="bg-green-800 text-white">
                                     <td className="px-20 py-2 text-right font-bold" colSpan="2">Sub Total (Rs.) :</td>
                                     <td className="px-6  font-bold">Rs. {totalIncome.toFixed(2)}</td>
                                     <td className="px-6  font-bold"></td>
                                 </tr>
-                                <tr className="bg-blue-800 text-white">
+                                <tr className="bg-green-800 text-white">
                                     <td className="px-20  text-right font-bold" colSpan="2">Total (Rs.) :</td>
                                     <td className="px-6 font-bold">Rs. {totalIncome.toFixed(4)}</td>
                                     <td className="px-6 font-bold"></td>
