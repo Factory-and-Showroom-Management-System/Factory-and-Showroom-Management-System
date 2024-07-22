6ab8b4a1eaed39b757ea89c8d628421fa8597bdb
Lahiru Prasad <104630433+Lahiru-LK@users.noreply.github.com>
2024-07-22 20:35:24 +0530

    color change and dashboard chart remove

--- Front/src/pages/Finance/Salary/Addition/Addition.jsx
+++ Front/src/pages/Finance/Salary/Addition/Addition.jsx
@@ -206,7 +206,7 @@ export default function Addition() {
 
                 <div className=''>
 
-                    <h1 className="  text-3xl text-blue-500">Addition Table</h1>
+                    <h1 className="  text-3xl text-green-700">Addition Table</h1>
 
                     <div className="pt-2 pb-2 bg-white">
                         <div className="relative ">
@@ -247,7 +247,7 @@ export default function Addition() {
                     </div>
                     <div className="relative overflow-x-auto sm:rounded-lg">
                     <table className="w-full text-sm text-left text-blue-100 dark:text-blue-100">
-                        <thead className="text-xs text-white uppercase bg-blue-600 border-b border-blue-400">
+                        <thead className="text-xs text-white uppercase bg-green-600 border-b border-blue-400">
                             <tr>
                                 <th scope="col" className="px-6 py-7">ID</th>
                                 <th scope="col" className="px-6 py-3">User ID</th>
@@ -260,7 +260,7 @@ export default function Addition() {
                         </thead>
                         <tbody>
                             {currentRows.map((item) => (
-                                <tr key={item.id} className="bg-blue-500 text-white border-b border-blue-400 hover:bg-blue-400">
+                                <tr key={item.id} className="bg-[#cdf8da] text-black border-b border-[#4bf885] hover:bg-[#a1f0c6]">
                                     <td className="px-6 py-4">{item.id}</td>
                                     <td className="px-6 py-4">{item.userId}</td>
                                     <td className="px-6 py-4">{item.name}</td>
@@ -273,14 +273,14 @@ export default function Addition() {
                                     </td>
                                 </tr>
                             ))}
-                            <tr className="bg-blue-800 text-white">
+                            <tr className="bg-green-800 text-white">
                                 <td className="px-20 py-2 text-right font-bold" colSpan="3">Sub Total (Rs.) :</td>
                                 <td className="px-6  font-bold">Rs. {totalOT.toFixed(2)}</td>
                                 <td className="px-6  font-bold">Rs. {totalAllowance.toFixed(2)}</td>
                                 <td className="px-6  font-bold">Rs. {totalAddition.toFixed(2)}</td>
                                 <td className="px-6  font-bold"></td>
                             </tr>
-                            <tr className="bg-blue-800 text-white ">
+                            <tr className="bg-green-800 text-white ">
                                 <td className="px-20 py-2 text-right font-bold " colSpan="3">Total (Rs.) :</td>
                                 <td className="px-6 font-bold">Rs. {totalAddition.toFixed(4)}</td>
                                 <td className="px-6 font-bold"></td>
